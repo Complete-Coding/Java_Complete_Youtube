2e8aca9725088b2671e766a65257907e8dcc8137
Prashant Jain <prashant@knowledgegate.in>
2023-12-13 13:55:15 +0530

    Java Basics

---
+++ 2 Java Basics/1 Leacture/FirstProgram.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class FirstProgram {
+  public static void main(String[] args) {
+    System.out.println("Welcome");
+    System.out.println("KG");
+    System.out.println("Coding");
+  }
+}
---
+++ 2 Java Basics/1 Leacture/FirstProject/src/Main.java
@@ -0,0 +1,7 @@
+package FirstProject.src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome KG Coding");
+        System.out.println("asdfasdf");
+    }
+}

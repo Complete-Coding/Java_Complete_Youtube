ec747c820b16d89dca44dbf0190bb645475ed058
SumitBangar83 <146200066+SumitBangar83@users.noreply.github.com>
2024-03-22 19:25:52 +0530

    Update 33 GCD.java

--- Programming Challenge/src/33 GCD.java
+++ Programming Challenge/src/33 GCD.java
@@ -12,24 +12,39 @@ class GCD {
         System.out.println("GCD of the numbers is: " + gcd);
     }
 
-    public static int gcd(int num1, int num2) {
+    public static int gcd(int First, int Second) {
+        // Initialize the greatest common divisor (gcd) to 1 || For eg - {12,20} {11,30} {11,33} 
         int gcd = 1;
+        
+        // Check if one number is a multiple of the other
+        if (Second % First == 0) {
+            return First; // Return First if it divides Second evenly
+        } else if (First % Second == 0) {
+            return Second; // Return Second if it divides First evenly
+        }// {12,20} {11,30}          {11,33} --> 3 
+        
+        // Start checking divisibility from 2
         int i = 2;
-        int least = least(num1, num2);
-        while (i <= least) {
-            if (num1 % i == 0 && num2 % i == 0) {
-                gcd = i;
-            }
-            i++;
-        }
-        return gcd;
-    }
 
-    public static int least(int num1, int num2) {
-        if (num1 < num2) {
-            return num1;
+        // Iterate through numbers from 2 to 10 to find the greatest common divisor
+        while (i <= 10) {
+            // Check if both First and Second are divisible by the current number (i)
+            if (First % i == 0 && Second % i == 0) {
+                gcd = i; // Update gcd if i is a common divisor
+            }
+            i++; // Move to the next number
+        }// {11,30}-->0       {12,20} ---> 2 
+
+        // Check if a common divisor was found
+        if (gcd == 1) {
+            /* As the gcm is still one, In this case both or one of them will be prime otherwise they will get be  
+            divisible by any number from 2 to 10 
+            */
+            return 0;
+        //  {11,30} ---> 0
         } else {
-            return num2;
-        }
+            return gcd; // Return the greatest common divisor
+        }// {12,20}--> 2
     }
 }
+
